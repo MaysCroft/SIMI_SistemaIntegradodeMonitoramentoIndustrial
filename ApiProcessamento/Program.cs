fd23983141db55560ca335d3a1594903be81a68e
May_Croft <mayconpilar19@gmail.com>
2026-04-25 15:28:02 -0300

    Implementação de novo sensor de níve de água, implementação para geração de documentação e criação de banco de dados.

--- ApiProcessamento/Program.cs
+++ ApiProcessamento/Program.cs
@@ -1,4 +1,6 @@
 using ApiProcessamento.Config;
+using ApiProcessamento.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    options.IncludeXmlComments(xmlPath);
+});
+
+// Configuração do Entity Framework Core para usar o SQLite como banco de dados
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=simibd.db"));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Injeção de dependencia para configuração
 builder.Services.Configure<ApiConfig>(
